b8666bb5882a618476be9de4c5b8ce9f496fa9b9
zhouxuecheng2016 <zhouxuecheng2016@gmail>
2017-12-24 04:58:55 +0800

    eshop cache

--- eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/Application.java
+++ eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/Application.java
@@ -2,6 +2,7 @@ package com.roncoo.eshop.cache.ha;
 
 import javax.sql.DataSource;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -9,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -23,13 +25,13 @@ import com.roncoo.eshop.cache.ha.filter.HystrixRequestContextFilter;
 @ComponentScan
 @MapperScan("com.roncoo.eshop.cache.ha.mapper")
 public class Application {
- 
+
     @Bean
-    @ConfigurationProperties(prefix="spring.datasource")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return new org.apache.tomcat.jdbc.pool.DataSource();
     }
-    
+
     @Bean
     public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
@@ -38,22 +40,29 @@ public class Application {
         sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
         return sqlSessionFactoryBean.getObject();
     }
- 
+
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
-    
+
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
-    	FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(
-    			new HystrixRequestContextFilter());
-    	filterRegistrationBean.addUrlPatterns("/*");
-    	return filterRegistrationBean;
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(
+                new HystrixRequestContextFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
-    
+
+    @Bean
+    public ServletRegistrationBean indexServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
 }
